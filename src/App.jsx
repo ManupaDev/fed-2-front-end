c0c9a182bea21e75022410378693c748836a4592
ManupaDev <manupadevsuite@gmail.com>
2025-04-21 17:43:42 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Button from "./Button";
+import Manupa from "./Manupa";
+const App = () => {
+  return (
+    <>
+      <h1>Hello World</h1>      
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Manupa />
+    </>
+  );
+};
+
+export default App;
