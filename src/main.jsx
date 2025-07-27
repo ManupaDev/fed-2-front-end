e61b033c892dee5ffc8232b97a88de5eff0aaf3c
ManupaDev <manupadevsuite@gmail.com>
2025-07-27 16:37:34 +0530

    Remove unnecessary blank line in main routing for improved code clarity

--- src/main.jsx
+++ src/main.jsx
@@ -44,7 +44,6 @@ createRoot(document.getElementById("root")).render(
                   <Route path="checkout" element={<CheckoutPage />} />
                   <Route path="payment" element={<PaymentPage />} />
                   <Route path="complete" element={<CompletePage />} />
-                  
                 </Route>
               </Route>
               <Route element={<ProtectedLayout />}>
